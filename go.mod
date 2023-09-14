171c8f7327775605085683b9f9e738a45200b93e
sudharsan-00 <sudharsan.cd22@bitsathy.ac.in>
2023-09-14 16:15:55 +0530

    secound

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sudharsan-00/puppy
+
+go 1.20
---
+++ puppy.go
@@ -0,0 +1,9 @@
+package puppy
+
+func Bark() string {
+	return "good morning"
+}
+
+func Barks() string {
+	return "had u eat lunch"
+}
