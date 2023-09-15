6b857dbb80b2763a6108d907569723418609a0a6
sudharsan-00 <sudharsan.cd22@bitsathy.ac.in>
2023-09-15 14:02:47 +0530

    fourth

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sudharsan-00/puppy
 
 go 1.20
+
+require github.com/sudharsan-00/dog v0.0.0-20230915045202-c329889bbc95 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,20 @@
 package puppy
 
+import (
+	"github.com/sudharsan-00/dog"
+)
 func Bark() string {
 	return "good morning"
 }
 
 func Barks() string {
 	return "had u eat lunch"
+}
+
+func Bigbark() string {
+	return dog.Whengrownup(Bark())
+}
+
+func Bigbarks() string {
+	return dog.Whengrownup(Barks())
 }
