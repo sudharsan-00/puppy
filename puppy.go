12027dc42786c3d988c8ddc090407c5cbf80f319
sudharsan-00 <sudharsan.cd22@bitsathy.ac.in>
2023-12-06 10:50:34 +0530

    from1

--- puppy.go
+++ puppy.go
@@ -1,10 +1,13 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/sudharsan-00/dog"
 )
-
-
+func from1(){
+	fmt.Println("I am from version 1.0.0")
+}
 func Bark() string {
 	return "good morning"
 }
